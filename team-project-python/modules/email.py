0a19e978959b7c6b802e4cce3f942cdf50eeafaa
karimorozova13 <k.morozova@tiomarkets.com>
2023-12-19 15:55:16 +0200

    add structure of the project

---
+++ team-project-python/modules/email.py
@@ -0,0 +1,5 @@
+from .field import Field
+
+
+class Email(Field):
+    pass
---
+++ team-project-python/modules/field.py
@@ -0,0 +1,8 @@
+class Field:
+    def __init__(self,val):
+        self.value = val
+    def __str__(self) -> str:
+        return str(self.value)
+
+
+    
