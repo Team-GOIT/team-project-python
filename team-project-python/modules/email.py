1ad90a4a44f7ef7a220189b6b297e7f0fba4e649
ShinVal98 <75795141+ShinVal98@users.noreply.github.com>
2023-12-19 22:58:05 +0100

    created class Address, Birthday, Email, Name, Phone
    All the classes are created with validation.

--- team-project-python/modules/email.py
+++ team-project-python/modules/email.py
@@ -1,5 +1,25 @@
 from .field import Field
-
+import re
 
 class Email(Field):
-    pass
+    #створення класу Email, де відбувається валідація введеної пошти на стандартний формат електроної пошти
+
+    def __init__(self, email = None):
+        self.email = self.validate_email(email) if email else None
+
+    def validate_email(self, email):
+        if not email:
+            return None
+        
+        pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+
+        if re.match(pattern, email):
+            return email
+        else:
+            raise ValueError('Please provide a valid email address.')
+
+    def str(self):
+        if self.email:
+            return f'Email: {self.email}'
+        else:
+            return 'No email set.'
--- team-project-python/modules/phone.py
+++ team-project-python/modules/phone.py
@@ -1,5 +1,26 @@
 from .field import Field
+import re
 
 class Phone(Field):
-     def __init__(self,name) -> None:
-            super().__init__(name)
+     #створення класу Phone, де ми перевіряємо чи передан правильний формат телефону
+
+     def __init__(self, number = None):
+            self.number = self.validate_phone_number(number) if number else None
+
+     def validate_phone_number(self, number):
+           if not number:
+                 return None
+           
+           # перевірка чи номер телефону починається з +, далі (код країни) та сам номер телефону
+           pattern = r'^\+(\(\d{1,3}\)|\d{1,3})\s\d{6,}$' 
+
+           if re.match(pattern, number):
+                 return number
+           else:
+                 raise ValueError ('Not correct format. Please use + (country code) phone number.')
+
+     def __str__(self):
+           if self.number:
+                 return f'Phone number: {self.number}'
+           else:
+                 return 'No phone number set.'
