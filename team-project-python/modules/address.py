f17700cac8da89eec42f59cfdcbd40a633babe1b
ShinVal98 <75795141+ShinVal98@users.noreply.github.com>
2023-12-20 10:24:25 +0100

    created class Address, Birthday, Email, Name, Phone (#3)
    All the classes are created with validation.

--- team-project-python/modules/address.py
+++ team-project-python/modules/address.py
@@ -1,6 +1,30 @@
 from .field import Field
 
 class Address(Field):
-    def __init__(self,name) -> None:
-        super().__init__(name)
+    #створюємо клас Address та перевіряємо чи адреса обов'язково містить країну та місто, а також чи поштовий індекс складається з 5ти цифр
+    
+    def __init__(self, country='', city='', street='', house_number='', postal_code=''):
+        self.country = country
+        self.city = city
+        self.street = street
+        self.house_number = house_number
+        self.postal_code = postal_code
+        self.validate()
+
+    def validate(self):
+        if not self.country or not self.city:
+            raise ValueError('Country and city is required for an address.')
+        
+        if self.postal_code and (not self.postal_code.isdigit() or len(self.postal_code) != 5):
+            raise ValueError('Postal code should be a 5-digit number.')
+
+    def __str__(self):
+        address_parts = [self.country, self.city, self.street, self.house_number, self.postal_code]
+        formatted_address = ', '.join(filter(None, address_parts))
+
+        if formatted_address:
+            return f'Address: {formatted_address}'
+        else:
+            return 'No address provided.'
+
         
