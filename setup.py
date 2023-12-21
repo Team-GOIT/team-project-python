b3cb996e3903b9135785b2f745d33c5c2b2ffcfa
Roman <63170330+RomaYaki@users.noreply.github.com>
2023-12-21 14:03:37 +0200

    added Licens and setup

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-
-print(setup);
+setup(
+    name="AssistantProjGOIT",
+    version="1.0",
+    description='Assistant bot which can write to change data',
+    url='https://github.com/Team-GOIT/team-project-python',
+    author='Karine Morozova, Valerie Matviichuk, Anton Olienykov, Oleksandr G, Yakymovych Roman',
+    author_email='k.morozova@tiomarkets.com, lera.matviichuk01@gmail.com, olieinykov@gmail.com, roma.yakimovich@yahoo.com, ',
+    packages=find_packages(),
+    setup_requires=['prettytable'],
+    install_requires=['prettytable']
+)
