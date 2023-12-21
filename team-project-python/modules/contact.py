a5a26202692f767db121a5289c6b6727211bceda
oleksandrcore <153992542+oleksandrcore@users.noreply.github.com>
2023-12-21 11:42:16 +0200

    Add contact class (#7)

--- team-project-python/modules/contact.py
+++ team-project-python/modules/contact.py
@@ -1,9 +1,67 @@
 from .name import Name
 from .address import Address
+from .phone import Phone
+from .email import Email
+from .birthday import Birthday
+
 
 class Contact:
-    def __init__(self, name, address) -> None:
-        self.name = Name(name)
-        self.address = Address(address)
+    def __init__(self, name: str) -> None:
+        self._name = Name(name)
+        self._phone = None
+        self._email = None
+        self._address = None
+        self._birthday = None
+
     def __str__(self) -> str:
-        return f"{self.name} is living {self.address}"
+        info_array = [f"Contact {self._name}", self._phone, self._email,
+                      self._address, self._birthday]
+        return ','.join(filter(lambda n: n, info_array))
+
+    @property
+    def name(self) -> Name | None:
+        return self._name
+
+    @property
+    def phone(self) -> Phone | None:
+        return self._phone
+
+    @property
+    def email(self) -> Email | None:
+        return self._email
+
+    @property
+    def address(self) -> Address | None:
+        return self._address
+
+    @property
+    def birthday(self) -> Birthday | None:
+        return self._birthday
+
+    def add_or_edit_phone(self, value: str) -> None:
+        self._set_field('_phone', Phone(value), "Phone successfully added/updated")
+
+    def add_or_edit_email(self, value: str) -> None:
+        self._set_field('_email', Email(value), "Email successfully added/updated")
+
+    def add_or_edit_address(self, value: str) -> None:
+        self._set_field('_address', Address(value), "Address successfully added/updated")
+
+    def add_or_edit_birthday(self, value: str) -> None:
+        self._set_field('_birthday', Birthday(value), "Birthday successfully added/updated")
+
+    def remove_phone(self) -> None:
+        self._set_field('_phone', None, "Phone successfully removed")
+
+    def remove_email(self) -> None:
+        self._set_field('_email', None, "Email successfully removed")
+
+    def remove_address(self) -> None:
+        self._set_field('_address', None, "Address successfully removed")
+
+    def remove_birthday(self) -> None:
+        self._set_field('_birthday', None, "Birthday successfully removed")
+
+    def _set_field(self, field, value, success_message):
+        setattr(self, field, value)
+        print(success_message)
