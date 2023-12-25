72c7ad0b58e5a9ec4e02ae8a1f2bf4712a32ec10
Roman <63170330+RomaYaki@users.noreply.github.com>
2023-12-25 12:53:06 +0200

    change setup

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="AssistantProjGOIT",
-    version="1.0",
-    description='Assistant bot which can write to change data',
-    url='https://github.com/Team-GOIT/team-project-python',
-    author='Karine Morozova, Valerie Matviichuk, Anton Olienykov, Oleksandr G, Yakymovych Roman',
-    author_email='k.morozova@tiomarkets.com, lera.matviichuk01@gmail.com, olieinykov@gmail.com, roma.yakimovich@yahoo.com, ',
+    name="AssistantTeamProjectGOIT",
+    version="1.1",
+    description="Assistant bot which can write to change data",
+    url="https://github.com/Team-GOIT/team-project-python",
+    author="Karine Morozova, Valerie Matviichuk, Anton Olienykov, Oleksandr G, Yakymovych Roman",
+    author_email="k.morozova@tiomarkets.com, lera.matviichuk01@gmail.com, olieinykov@gmail.com, roma.yakimovich@yahoo.com, ",
     packages=find_packages(),
-    setup_requires=['prettytable'],
-    install_requires=['prettytable', 'prompt-toolkit', 'twilio']
+    setup_requires=["prettytable"],
+    install_requires=["prettytable", "prompt-toolkit", "twilio"],
 )
