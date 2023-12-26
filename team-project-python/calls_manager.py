1bc7bfd4505f77f25e58b8feae10f2c10b65aa54
Anton Olieinykov <olieinykov@gmail.com>
2023-12-26 15:54:58 +0200

    Update twilio token

--- team-project-python/calls_manager.py
+++ team-project-python/calls_manager.py
@@ -2,7 +2,7 @@ from twilio.rest import Client
 
 # Move to environment variables
 account_sid = "ACd04d5632398fd0dfab997a798e3070a6"
-auth_token = "602431b4ae7c559d331918bc77c47d9f"
+auth_token = "675aea8ca123981e80b6823247f6b332"
 from_number = "+14082284738"
 
 twilio_client = Client(account_sid, auth_token)
@@ -34,9 +34,6 @@ def voice_message(phone_number, text):
             to=phone_number,
             from_=from_number,
         )
-
-        # Print the call SID
-        print(f"Call SID: {call.sid}")
 
         if call.sid:
             print(f"Calling '{phone_number}...'")
